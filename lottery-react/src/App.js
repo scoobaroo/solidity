670bf5f6eb2e519da5c53be8897a6fd4dbef365e
Eric Han <ehan@ISSI.COM>
2018-05-23 18:21:34 -0700

    first commit

---
+++ lottery-react/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+
+    state = { manager : '' , players : [], balance : ''};
+
+    async componentDidMount(){
+        const manager = await lottery.methods.manager().call();
+        
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+        this.setState({manager , players, balance});
+
+    }
+
+    render() {
+        console.log(web3);
+        return ( 
+            <div>
+                <h2>Lottery Contract</h2>
+                <p>
+                    This contract is managed by {this.state.manager}.
+                    This contract has {this.state.players.length} people entered competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
