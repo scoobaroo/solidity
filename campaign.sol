329ed84a7f02cf12504da80e1cb26d8a37706486
Eric Han <ehan@ISSI.COM>
2018-05-29 17:46:29 -0700

    added campaign.sol

---
+++ campaign.sol
@@ -0,0 +1,63 @@
+
+pragma solidity ^0.4.17;
+
+contract Campaign {
+    
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address=>bool) approvals;
+    }
+    
+    uint public approversCount;
+    address public manager;
+    mapping(address=>bool) public approvers;
+    uint public minimumContribution;
+    Request[] public requests;
+    
+    modifier restricted(){
+        require(msg.sender==manager);
+        _;
+    }
+    
+    constructor(uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        request.approvalCount++;
+        request.approvals[msg.sender]=true;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted{
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            approvalCount: 0,
+            complete: false
+        });
+        requests.push(newRequest);
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(!request.complete);
+        require(request.approvalCount > (approversCount/2));
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
